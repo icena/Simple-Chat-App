61c232bbd845da4d8e758d1df2f0b52040af5a52
jpistomo <jpistomo@gmail.com>
2022-04-05 21:03:02 +0700

    Initial Commit

---
+++ Simple Chat App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Chat App
+//
+//  Created by JP on 05/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
